53fb76c08a0c984250d19b03ff9bdbebf50bd685
Zakariya Sattar <zakariyasattar03@gmail.com>
2024-04-11 22:40:08 -0500

    test netlify parcel

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
+import zipcodes from 'zipcodes';
 
+console.log("hello");
 
+// Ensure the document is fully loaded before setting up handlers
+document.addEventListener('DOMContentLoaded', function() {
+    // Setup your event listeners or other initialization logic here
+    document.getElementById("search").addEventListener('click', search);
+});
+
+// Define 'search' in the global scope if it needs to be accessible globally
 function search() {
-  var treatment = document.getElementById("treatment").value;
-  var zip_code = document.getElementById("zip-code").value;
+    var treatment = document.getElementById("treatment").value;
+    var zip_code = document.getElementById("zip-code").value;
 
-  import('zipcodes').then(zipcodes => {
-  // Use the zipcodes library here...
-    console.log(zipcodes.lookupByZip('10001')); // Example usage
-  }).catch(error => {
-    console.error('Error loading zipcodes library:', error);
-  });
-
-
-  var rad = zipcodes.radius(parseInt(zip_code), 50);
-  console.log(rad);
-
+    var rad = zipcodes.radius(parseInt(zip_code), 50);
+    console.log(rad);
 }
