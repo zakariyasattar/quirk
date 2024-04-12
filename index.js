abe40ac14fc3d7b3604470790660b80f820373c0
Zakariya Sattar <zakariyasattar03@gmail.com>
2024-04-12 09:20:46 -0500

    data filter working

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import zipcodes from 'zipcodes';
-
-console.log("hello");
+import Papa from 'papaparse';
+import * as d3 from 'd3';
 
 // Ensure the document is fully loaded before setting up handlers
 document.addEventListener('DOMContentLoaded', function() {
@@ -13,6 +13,17 @@ function search() {
     var treatment = document.getElementById("treatment").value;
     var zip_code = document.getElementById("zip-code").value;
 
-    var rad = zipcodes.radius(parseInt(zip_code), 30);
-    console.log(rad);
+    var rad = zipcodes.radius(parseInt(zip_code), 15);
+
+    const dataloc = require('./public/data.csv');
+    d3.csv(dataloc).then((data) => {
+      const filteredData = data.filter(row => rad.includes(row.zip_code) && (row.service).indexOf(treatment) != -1);
+
+      localStorage.setItem("filteredData", JSON.stringify(filteredData));
+      loadResults()
+    })
+}
+
+function loadResults() {
+  
 }
