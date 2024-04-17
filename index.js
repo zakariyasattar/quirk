fd1e238b9777ed4b3a4c3750cb73d168c436dab3
Zakariya Sattar <zakariyasattar03@gmail.com>
2024-04-17 17:12:04 -0400

    basic display working

--- index.js
+++ index.js
@@ -1,56 +1,104 @@
+if (typeof global === "undefined"){global=window;}
+
 import zipcodes from 'zipcodes';
 import Papa from 'papaparse';
+import axios from 'axios';
 import * as d3 from 'd3';
 
-// Ensure the document is fully loaded before setting up handlers
 document.addEventListener('DOMContentLoaded', function() {
-    // Setup your event listeners or other initialization logic here
     document.getElementById("search").addEventListener('click', search);
-    document.getElementById("plan_search").addEventListener('keyup', filterPlans);
+    document.getElementById("plan_search").addEventListener('onfocus', filterPlans);
 });
 
-// Define 'search' in the global scope if it needs to be accessible globally
 function search() {
     var treatment = document.getElementById("treatment").value;
     var zip_code = document.getElementById("zip-code").value;
 
     var rad = zipcodes.radius(parseInt(zip_code), 10);
 
-    const dataloc = require('./public/data.csv');
-    d3.csv(dataloc).then((data) => {
-      const filteredData = data.filter(row => rad.includes(row.zip_code) && (row.service).indexOf(treatment) != -1);
-      console.log(filteredData);
-
-      localStorage.setItem("filteredData", JSON.stringify(filteredData));
-      loadResults()
-    })
+    parse(treatment, rad);
 }
 
-function loadResults() {
+function parse(treatment, zips) {
+  var indices = ["provider","health_system","service","code","code_type","plan_raw","rate","medicare_provider_id","zip_code","street_address","city","state","county","acute_care_facility","hospital_overall_rating","hospital_type","total_beds","npi","compliance_score"];
+
+  var GetFileBlobUsingURL = function (url, convertBlob) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.responseType = "blob";
+    xhr.addEventListener('load', function() {
+        convertBlob(xhr.response);
+    });
+    xhr.send();
+  };
+  var blobToFile = function (blob, name) {
+    blob.lastModifiedDate = new Date();
+    blob.name = name;
+    return blob;
+  };
+  var GetFileObjectFromURL = function(filePathOrUrl, convertBlob) {
+     GetFileBlobUsingURL(filePathOrUrl, function (blob) {
+        convertBlob(blobToFile(blob, 'data.csv'));
+     });
+  };
+
+  var FileURL = require('./static/data.csv');
+
+  var parsedData = [];
+  var preferred_insurance;
+
+  if(localStorage.getItem("preferred_plan") == null) {
+    preferred_insurance = "List Price";
+  }
+  else {
+    preferred_insurance = localStorage.getItem("preferred_plan");
+  }
+
+  GetFileObjectFromURL(FileURL, function (fileObject) {
+     Papa.parse(fileObject, {
+       worker: true,
+       step: function(results) {
+         var hospital_zip = results.data[indices.indexOf("zip_code")];
+         var service = results.data[indices.indexOf("service")];
+         var insurance = results.data[indices.indexOf("plan_raw")];
+         var withinRadius = zips.includes(hospital_zip);
+
+         if(withinRadius && service.toLowerCase().indexOf(treatment.toLowerCase()) != -1 && insurance == preferred_insurance) {
+           var hospital = results.data[0];
+           var address = results.data[indices.indexOf("street_address")];
+           var cash_price = results.data[indices.indexOf("rate")];
+
+           var details = {
+             "service": service,
+             "hospital": hospital,
+             "address": address,
+             "rate": cash_price,
+             "insurance": insurance
+           };
+
+           parsedData.push(details);
+         }
+       },
+       complete: function() {
+         loadResults(parsedData);
+       }
+     });
+  });
+
+}
+
+function loadResults(data) {
   var main = document.getElementById("main");
   var results = document.getElementById("results");
 
   main.style.display = "none";
   results.style.display = "block";
 
-  var data = JSON.parse(localStorage.getItem("filteredData"));
   var options = {};
 
   for(var x in data) {
-    addPlanOption(data[x].plan_raw);
-
-    if(data[x].plan_raw == localStorage.getItem("preferred_plan") || data[x].plan_raw == "Cash Price") {
-      // if(options[data[x].provider + ";" + data[x].service] != null) {
-      //   var priceOptions = options[data[x].provider + ";" + data[x].service];
-      //   priceOptions.push(data[x].rate);
-      //   options[data[x].provider + ";" + data[x].service] = priceOptions;
-      // }
-      // else {
-      //   options[data[x].provider + ";" + data[x].service] = [];
-      // }
-      createResult(data[x]);
-      console.log(data[x]);
-    }
+    addPlanOption(data[x].insurance);
+    createResult(data[x]);
   }
   console.log(options);
 
@@ -74,7 +122,7 @@ function createResult(data) {
   const footer = document.createElement('div');
 
   // Set text contents and attributes
-  hospitalName.textContent = data.provider;
+  hospitalName.textContent = data.hospital;
   cashPrice.textContent = `Cash Price: ${data.rate}`;
   yourPrice.textContent = `Your Price: ${data.rate}`;
   serviceTitle.textContent = data.service;
