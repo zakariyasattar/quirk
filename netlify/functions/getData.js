eb1a2eb086a80f52d6a19b204d6dce14c22bbe45
Zakariya Sattar <zakariyasattar03@gmail.com>
2024-05-21 18:13:37 -0500

    test db

--- netlify/functions/getData.js
+++ netlify/functions/getData.js
@@ -1,53 +1,32 @@
-// netlify/functions/getDocuments.js
-const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
 
-const uri = "mongodb+srv://quirkhealth:gMBBJZYbw346wpk9@quirk-main.yygw4d6.mongodb.net/?retryWrites=true&w=majority&appName=quirk-main";
-
-let cachedDb = null;
-
-const connectToDatabase = async () => {
-    if (cachedDb) {
-        return cachedDb;
-    }
-
-    const client = await mongoose.connect(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-
-    cachedDb = client;
-    return client;
-};
-
-const yourSchema = new mongoose.Schema({
-    // Define your schema here
-    provider: { type: String, required: true },
-    street_address: { type: String, required: true },
-    city: { type: String, required: true },
-    state: { type: String, required: true },
-    code: { type: String, required: true },
-    service: { type: String, required: true },
-    cash_rate: { type: mongoose.Schema.Types.Mixed, required: true },
-    plans: [String]
-
-});
-
-const Model = mongoose.model('Model', yourSchema);
-
-exports.handler = async (event, context) => {
-    context.callbackWaitsForEmptyEventLoop = false;
+exports.handler = async function(event, context) {
+    const uri = "mongodb+srv://quirkhealth:gMBBJZYbw346wpk9@quirk-main.yygw4d6.mongodb.net/?retryWrites=true&w=majority&appName=quirk-main";
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
     try {
-        await connectToDatabase();
-        const documents = await Model.find().exec();
+        await client.connect();
+        const database = client.db('quirk');
+        const collection = database.collection('main');
+
+        const documents = await collection.find({}).toArray();
         return {
             statusCode: 200,
+            headers: {
+                "Content-Type": "application/json"
+            },
             body: JSON.stringify(documents),
         };
     } catch (error) {
+        console.error(error);
         return {
             statusCode: 500,
-            body: JSON.stringify({ error: 'Failed to fetch documents' }),
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ message: 'Internal Server Error' }),
         };
+    } finally {
+        await client.close();
     }
 };
