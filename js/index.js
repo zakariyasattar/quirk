347606fb84ed80e06c8365a8aadaeabf52e8fcb0
Zakariya Sattar <zakariyasattar03@gmail.com>
2024-05-21 19:57:35 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -17,6 +17,6 @@ document.addEventListener('DOMContentLoaded', function() {
       document.getElementById("main").style.display = "block";
     });
 
-    loadResults();
+    // loadResults();
     // makeAppointment();
 });
