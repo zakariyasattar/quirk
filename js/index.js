07d1c5c369dd55b10faf1fd7a26a8c4c72fe741b
Zakariya Sattar <zakariyasattar03@gmail.com>
2024-05-30 10:24:17 -0500

    drodown working

--- js/index.js
+++ js/index.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("zip-code").addEventListener('input', function (evt) {
         var value = this.value;
 
-        if(isNaN(value)) {
+        if(isNaN(value) && !(value.charAt(0) >= '0' && value.charAt(0) <= '9')) {
             document.getElementById("dropdown").style.display = "block";
             filterFunction();
         }
@@ -37,22 +37,98 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    document.getElementById("zip-code").addEventListener('focus', function (evt) {
+
+      if(isNaN(evt.target.value)) {
+        document.getElementById('dropdown').style.display = "block";
+        filterFunction();
+      }
+
+    });
+
+    document.addEventListener('mousedown', function(event) {
+      var dropdown = document.getElementById('dropdown');
+      if (!dropdown.contains(event.target)) {
+        dropdown.style.display = 'none';
+      }
+    });
+
+    populateProviders();
+
     // loadResults();
     // makeAppointment();
 });
+
+function populateProviders() {
+  var providers = [
+    'Advocate Christ Medical Center',
+    'Advocate Condell Medical Center',
+    'Advocate Good Samaritan Hospital',
+    'Advocate Good Shepherd Hospital',
+    'Advocate Illinois Masonic Medical Center',
+    'Advocate Lutheran General Hospital',
+    'Advocate Sherman Hospital',
+    'Advocate South Suburban Hospital',
+    'Advocate Trinity Hospital',
+    "Ann & Robert H. Lurie Children's Hospital of Chicago",
+    'Endeavor Health Evanston Hospital',
+    'Endeavor Health Swedish Hospital',
+    'Gottlieb Memorial Hospital',
+    'John H. Stroger, Jr. Hospital of Cook County',
+    'Loyola University Medical Center',
+    'MacNeal Hospital',
+    'Methodist Hospital',
+    'Northwestern Medicine Central DuPage Hospital',
+    'Northwestern Medicine Delnor Hospital',
+    'Northwestern Medicine Huntley Hospital',
+    'Northwestern Medicine Kishwaukee Hospital',
+    'Northwestern Medicine Lake Forest Hospital',
+    'Northwestern Medicine Marianjoy Rehabilitation Hospital',
+    'Northwestern Medicine McHenry Hospital',
+    'Northwestern Medicine Palos Hospital',
+    'Northwestern Medicine Valley West Hospital',
+    'Northwestern Medicine Woodstock Hospital',
+    'Northwestern Memorial Hospital',
+    'Rush Copley Medical Center',
+    'University of Illinois Hospital & Clinics'
+  ]
+
+  var dropdown = document.getElementById("dropdown");
+
+  for(var provider of providers) {
+    var a = document.createElement('a');
+    a.textContent = provider;
+    a.onclick = function() {
+      document.getElementById("zip-code").value = provider;
+      document.getElementById("dropdown").style.display = "none";
+    };
+    dropdown.appendChild(a);
+  }
+}
 
 function filterFunction() {
   var input, filter, ul, li, a, i;
   input = document.getElementById("zip-code");
   filter = input.value.toUpperCase();
-  div = document.getElementById("dropdown");
+  var div = document.getElementById("dropdown");
   a = div.getElementsByTagName("a");
+
+  var hiddenCounter = 0;
+
   for (i = 0; i < a.length; i++) {
-    txtValue = a[i].textContent || a[i].innerText;
+    var txtValue = a[i].textContent || a[i].innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
       a[i].style.display = "";
     } else {
       a[i].style.display = "none";
+      hiddenCounter++;
     }
   }
+
+  if(hiddenCounter == a.length) {
+    var a = document.createElement('a');
+    a.textContent = "No Results";
+    document.getElementById("dropdown").appendChild(a);
+  }
+
 }
